cfb4323dd8cd5800a803d2bba81ace80a7922de6
Ashley Clarke <crashley92@gmail.com>
2019-11-30 14:14:41 -0800

    added some styling. still working on adding the notes feature

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,6 @@
-
 //when Get Articles is click, article info is pulled and notes button is generated.
-$(document).on('click','.submit',function(){
-  $.getJSON("/api/articles", function(data) {
+$(document).on('click', '#submit', function () {
+  $.getJSON("/api/articles", function (data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the article information on the page
@@ -9,7 +8,7 @@ $(document).on('click','.submit',function(){
       //creates button for notes
       const noteInput = $('<button>');
       noteInput.text('Article Notes');
-      noteInput.addClass('note-saved-button');
+      noteInput.addClass('take-note');
       $('#articles').append(noteInput);
     }
   });
@@ -17,63 +16,6 @@ $(document).on('click','.submit',function(){
 
 //when article notes is click, pop up for article notes is generated
 // Whenever someone clicks article notes
-$(document).on("click", ".note-saved-button", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/api/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function(data) {
-      console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
+$(document).on("click", ".take-note", function () {
+ console.log('hi');
 });
-
-// When you click the savenote button
-$(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/api/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
-    }
-  })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
-    });
-
-
-});
-
